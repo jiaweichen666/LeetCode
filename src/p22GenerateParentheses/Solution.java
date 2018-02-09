1d35487803521214ada8ca0bf27d473550d00abe
jiaweichen666 <1096448779@qq.com>
2018-02-09 09:45:32 +0800

    15th commit.

--- src/p22GenerateParentheses/Solution.java
+++ src/p22GenerateParentheses/Solution.java
@@ -9,17 +9,16 @@ public class Solution {//没想到，过两天再来写
         backtrack(list, "", 0, 0, n);
         return list;
     }
-
-    public void backtrack(List<String> list, String str, int open, int close, int max){
-
-        if(str.length() == max*2){
-            list.add(str);
+    public void backtrack(List<String> list,String string,int begin,int end,int max){
+        if (string.length() == max*2){//长度够了就添加
+            list.add(string);
             return;
         }
-
-        if(open < max)
-            backtrack(list, str+"(", open+1, close, max);
-        if(close < open)
-            backtrack(list, str+")", open, close+1, max);
+        if (begin < max){
+            backtrack(list,string+"(",begin+1,end,max);
+        }
+        if (end < begin){
+            backtrack(list,string+")",begin,end+1,max);
+        }
     }
 }
