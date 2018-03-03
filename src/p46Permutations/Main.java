9c9dc7016e22b1f490c787f154ac4aeaae3a3637
jiaweichen666 <1096448779@qq.com>
2018-03-03 17:06:18 +0800

    17.1th commit.

---
+++ src/p46Permutations/Main.java
@@ -0,0 +1,9 @@
+package p46Permutations;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int [] nums = {1,2,3};
+        System.out.println(solution.permute(nums));
+    }
+}
